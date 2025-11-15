99182e6c49ad7f5434a899e6080c40ec2120e009
Anett <126421398+NovakovaA@users.noreply.github.com>
2025-11-15 22:05:30 +0100

    Update plotter.m

--- plotter.m
+++ plotter.m
@@ -7,8 +7,8 @@ y2 = 0;
 for i = 1:length(values)
     newline = strtrim(values(i));
     if startsWith(newline, "G0") || startsWith(newline, "G1")
-        xStr = regexp(newline, 'X([-+]?[0-9]*\.?[0-9]+)', 'tokens');
-        yStr = regexp(newline, 'Y([-+]?[0-9]*\.?[0-9]+)', 'tokens');
+        xStr = regexp(newline, 'X([0-9]*\.?[0-9]+)', 'tokens'); % searches the text in newline for the pattern 'X<number>', the number can be decimal, the part after 'X' is captured as a token (number after X) and returned in `xStr`
+        yStr = regexp(newline, 'Y([0-9]*\.?[0-9]+)', 'tokens');
 
         if ~isempty(xStr)
             x2 = str2double(xStr{1}{1});
@@ -18,16 +18,16 @@ for i = 1:length(values)
         end
 
         if startsWith(newline, "G0")
-            %plot([x1, x2], [y1, y2], ':w', LineWidth= 2);
+            %plot([x1, x2], [y1, y2], ':w', LineWidth= 2); % how the lifted pen moves
         elseif startsWith(newline, "G1")
             plot([x1, x2], [y1, y2], pen_color, LineWidth= 2);
         end
-        axis([0 148 0 210])
+        axis([0 148 0 210]) % A5
 
 
         x1 = x2;
         y1 = y2;
-        pause(0.00001);
+        pause(0.001);
     end
 end
 end
